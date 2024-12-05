fd1b7c9b4fccf206aab162bd8693c7f741991bda
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2024-12-05 15:11:31 +0100

    Update podman-run.bash

--- podman-run.bash
+++ podman-run.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t react:demo .
 podman run --interactive --tty -p 9090:80 react:demo
-echo "browse http://localhost:9090/"
+echo "browse http://localhost:9090/?username=test"
